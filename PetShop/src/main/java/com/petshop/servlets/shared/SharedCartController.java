5abf256ee41bff224a2819e4ddae479a827fe1e5
baongoc02 <tranthibaongoc779152@gmail.com>
2022-12-12 22:06:15 +0700

    thêm cart controller và paymentpage controller

---
+++ PetShop/src/main/java/com/petshop/servlets/shared/SharedCartController.java
@@ -0,0 +1,26 @@
+package com.petshop.servlets.shared;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/gio-hang")
+public class SharedCartController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public SharedCartController() {
+    	super();
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/templates/shared/cart.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ PetShop/src/main/java/com/petshop/servlets/shared/SharedPaymentPageController.java
@@ -0,0 +1,28 @@
+package com.petshop.servlets.shared;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/thanh-toan")
+public class SharedPaymentPageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public SharedPaymentPageController() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/templates/shared/paymentpage.jsp").forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
